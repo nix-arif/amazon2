071dc34fbf7f260f8650f3a93e0fc55478f9f694
nix-arif <nix.arif@gmail.com>
2020-11-11 15:03:17 +0800

    Create Screen and Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
-import Product from "./components/Product"
-import data from "./data"
+
+import { BrowserRouter, Route } from "react-router-dom"
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
     return (
+		<BrowserRouter>
 		<div className="grid-container">
 			<header className="row">
 				<div>
@@ -14,14 +17,12 @@ function App() {
 				</div>
 			</header>
 			<main>
-				<div className="row center">
-					{data.products.map(product => (
-						<Product key={product._id} product={product} />
-					))}
-				</div>
+				<Route path="/product/:id" component={ProductScreen} />
+				<Route path="/" component={HomeScreen} exact />
 			</main>
 			<footer className="row center">All right reserved</footer>
 		</div>
+		</BrowserRouter>
     )
 }
 
